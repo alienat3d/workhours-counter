6e7b8e17efef78653a4c46a85add20588ff5bbb1
Al Zaplin <zaplin.dev@gmail.com>
2024-06-29 05:21:10 +0300

    feat(copying at clipboard & bubble-notify)

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 'use strict';
 
-const countHours = (...args) => {
-	const sum = args.reduce((sum, num) => sum + num);
-	const result = (sum / 60).toFixed(2);
-	return result;
-}
+const input = document.querySelector('.input');
+const result = document.querySelector('.result');
+const copy = document.querySelector('.copy__btn');
+const alert = document.querySelector('.copy__alert');
+
+let totalHours;
+
+// Adds up the minutes and calculates the hours from them
+input.addEventListener('input', () => {
+	console.dir(input);
+	const minutesArray = input.value.split(',');
+
+	const totalMinutes = minutesArray.reduce(function(sum, num) {
+		return sum + Number(num);
+	}, 0);
+
+	totalHours = (totalMinutes / 60).toFixed(2).toString().replace('.', ',');
+
+	result.textContent = totalHours;
+
+	return totalHours;
+})
+
+// Copy output to clipboard & show notification bubble:
+copy.addEventListener('click', () => {
+  navigator.clipboard.writeText(totalHours);
+  
+  alert.classList.add('show');
+
+  setTimeout(() => {
+    alert.classList.remove('show');
+  }, 3000);
+});
